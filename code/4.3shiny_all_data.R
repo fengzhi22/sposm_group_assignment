804ea2bdaf260d689db0faf0b00e2cedde823ea5
Sebastian Geschonke <sebastian.geschonke@gmail.com>
2020-01-25 10:46:34 +0100

    integration of the key ags linking to regional data, more statisitcal sources, consistency checks, #8 #12 #16 #17

---
+++ code/4.3shiny_all_data.R
@@ -0,0 +1,204 @@
+# ***********************************************************************************************
+#### installing, loading libraries ####
+library("utils")
+
+packages <- c("here", "shiny", "zip", "sf", "tmap", "tmaptools", "ggplot2", "dplyr", "shinydashboard")
+
+### install if necessary
+lapply(packages, 
+       function(x)
+       {
+         if(!(x %in% installed.packages()) | x %in% old.packages())
+         {
+           install.packages(x)  
+         }
+       })
+
+lapply(packages, require, character.only = T)
+
+# **********************************************************************************************
+#### D E F I N I T I O N S ####
+#rm(list=ls(all=TRUE))
+options(scipen = 999) # no scientific numbering
+subfolder <- zip_list(here("data", "raw", "shape_germany_bundesland_landkreis.zip"))$filename[1]
+subfolder <- gsub("/", "", subfolder, fixed = TRUE)
+file_ger_shape <- here("data", "raw", "shape_ger", subfolder, "vg2500", "vg2500_sta.shp")
+file_ger_shape_state <- here("data", "raw", "shape_ger", subfolder, "vg2500", "vg2500_lan.shp")
+file_ger_shape_county <- here("data", "raw", "shape_ger", subfolder, "vg2500", "vg2500_krs.shp")
+
+
+# ***********************************************************************************************
+#### load data ####
+# read all data files
+data_state <- read.csv2(here("data", "processed", paste0("data_state", ".csv")), row.names = NULL, encoding = "UTF-8")
+data_state_yearly <- read.csv2(here("data", "processed", paste0("data_state_yearly", ".csv")), row.names = NULL, encoding = "UTF-8")
+data_state_yearly_extended <- read.csv2(here("data", "processed", paste0("data_state_yearly_extended", ".csv")), row.names = NULL, encoding = "UTF-8")
+
+data_county <- read.csv2(here("data", "processed", paste0("data_county", ".csv")), row.names = NULL, encoding = "UTF-8")
+data_county_yearly <- read.csv2(here("data", "processed", paste0("data_county_yearly", ".csv")), row.names = NULL, encoding = "UTF-8")
+data_county_yearly_extended <- read.csv2(here("data", "processed", paste0("data_county_yearly_extended", ".csv")), row.names = NULL, encoding = "UTF-8")
+
+data_offshore <- read.csv2(here("data", "processed", paste0("data_offshore", ".csv")), row.names = NULL, encoding = "UTF-8")
+
+
+# read all shape files
+ger_shape <- st_read(file_ger_shape, options = "ENCODING=UTF-8", stringsAsFactors = FALSE)
+ger_shape_state <- st_read(file_ger_shape_state, options = "ENCODING=UTF-8", stringsAsFactors = FALSE)
+ger_shape_county <- st_read(file_ger_shape_county, options = "ENCODING=UTF-8", stringsAsFactors = FALSE)
+
+
+# some shapes need multiple entries of geometry
+# these duplicated entries will multiply the amount of facilities and power -> needs correction if merged too early
+# federal level: duplicate found
+#ger_shape$GEN[duplicated(ger_shape$GEN)]
+
+# state-level: no duplicates
+#ger_shape_state$GEN[duplicated(ger_shape_state$GEN)]
+
+# county-level: some duplicates (with different geometry information: adding later to one county)
+#ger_shape_county$GEN[duplicated(ger_shape_county$GEN)]
+# numbers of counties
+#nrow(ger_shape_county) - length(ger_shape_county$GEN[duplicated(ger_shape_county$GEN)])
+
+## prepare merge
+names(ger_shape_state)[names(ger_shape_state) == "GEN"] <- "Bundesland"
+names(ger_shape_county)[names(ger_shape_county) == "GEN"] <- "Landkreis"
+
+names(ger_shape_state)[names(ger_shape_state) == "RS"] <- "ags"
+names(ger_shape_county)[names(ger_shape_county) == "RS"] <- "ags"
+
+ger_shape_county$ags <- as.numeric(ger_shape_county$ags)
+ger_shape_state$ags <- as.numeric(ger_shape_state$ags)
+
+# ***********************************************************************************************
+#### merge shape file after aggregation ####
+## general maps for data exploration
+map_and_data_state <- inner_join(ger_shape_state, data_state, by = c("ags" = "ags_federal_state"))
+map_and_data_county <- inner_join(ger_shape_county, data_county, by = c("ags" = "ags_county"))
+# Landkreis "offshore" dropped
+
+# missing ags_federal_state dropped
+table(data_state$ags_federal_state, useNA = "always")
+data_state[is.na(data_state$ags_federal_state),]
+# related to offshore (always if ags_federal_state is missing)
+
+
+## ...
+
+
+# ***********************************************************************************************
+#### create server ####
+server <- function(input, output) {
+  
+  dataset <- reactive({
+    get(paste0("map_and_data_", input$geo_level))
+  })
+  
+  energy_source <- reactive({
+    dataset() %>% filter(EinheitenTyp == input$source)
+  })
+  
+  coloring <- reactive({
+    if (input$source == "Solareinheit"){"Greens"}else{
+      if (input$source == "Windeinheit"){"Blues"}
+    }
+  })
+  
+  title_legend <- reactive({
+    if (input$out_var == "n"){"Number of power plants"}else{
+      if (input$out_var == "sum"){"Power in kw(p)"}
+    }
+  })
+  
+  output$plot1 <- renderPlot({
+    
+    p1 <- ggplot(energy_source(), aes(geometry = geometry)) +
+      geom_sf(aes(fill = get(input$out_var))) +
+      scale_fill_gradient(low = "#B6FF52", high = "#3B5518", name = "Quantity") +
+      theme(axis.title = element_blank(),
+            axis.text = element_blank(),
+            axis.ticks = element_blank(),
+            panel.background = element_blank())
+
+    print(p1)
+    
+  })
+  
+  output$plot2 <- renderPlot({
+    
+    p2 <- tm_shape(energy_source()) +
+      tm_polygons(input$out_var, palette = coloring(), n = 10, title = title_legend()) +
+      tm_layout(legend.outside = TRUE) +
+      tm_layout(frame = FALSE)
+    
+    print(p2)
+    
+  })
+  
+}
+
+# ***********************************************************************************************
+#### create ui ####
+ui <- dashboardPage(
+  dashboardHeader(title = "Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Storyline", tabName = "storyline", icon = icon("dashboard")),
+      menuItem("Data", tabName = "data_explorer", icon = icon("th")),
+      menuItem("Reference", tabName = "reference", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "storyline",
+              h2("Do rich regions use more renewable energy in Germany? (for example)"),
+              div(class = "text",
+                  p("Because we do not have a story yet,", tags$b("the content is left blank intentionally.")),
+                  p("And I just put these sentences", tags$em("to test the codes."))
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "data_explorer",
+              fluidRow(
+                column(width = 4,
+                       fluidRow(
+                         column(width = 12,
+                                h3("German Power Plants Explorer", align = "center"),
+                                #titlePanel("German Power Plants Explorer"),
+                                sidebarPanel(width = 12,
+                                             selectInput('source', 'Energy Source', c("Solar Energy" = "Solareinheit", "Wind Energy" = "Windeinheit")),
+                                             selectInput('geo_level', 'Geographical Level', c("State" = "state", "County" = "county")),
+                                             selectInput('out_var', 'Output Variable', c("Number of power plants" = "n", "Sum of power plants' power" = "sum"))
+                                ),
+                         )
+                       )
+                       
+                ),
+                column(width = 8,
+                       mainPanel(
+                         h3("Germany in geographical zones"),
+                         plotOutput('plot2')#,
+                         #plotOutput('plot1')
+                       )
+                )
+              )
+      ),
+      
+      # Third tab content
+      tabItem(tabName = "reference",
+              h2("Reference"),
+              div(class = "list",
+                  tags$ul(
+                    tags$li(tags$b("Data"), ": German power plants raw data is downloaded from official register “Marktstammdatenregister” through following link:", tags$a(href="https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Energie/Unternehmen_Institutionen/ErneuerbareEnergien/ZahlenDatenInformationen/VOeFF_Registerdaten/DatenAb310119.zip", "https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Energie/", tags$br("Unternehmen_Institutionen/ErneuerbareEnergien/ZahlenDatenInformationen/VOeFF_Registerdaten/DatenAb310119.zip"))),
+                    p("The register includes the potential production of power plants in Germany including renewables like wind, solar and biomass as well as coal and nuclear. The source provides more than 100 variables for various purposes."),
+                    tags$li(tags$b("License"), ": What should we write about data license? Should we translate some relevant sections of the data usage policy of Bundesnetzagentur?")
+                  )
+              )
+      )
+    )
+  )
+)
+
+shinyApp(ui, server)
